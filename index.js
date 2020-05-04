22ca90a628023f0cf3f0885f09d3aeb013e241fd
gordela <giorgi.gordiashvili.1@btu.edu.ge>
2020-05-04 23:12:17 +0400

    s

--- index.js
+++ index.js
@@ -122,6 +122,16 @@ app.get("/", (req, res) => {
 // @route POST /upload
 // @desc  Uploads file to DB
 app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
   res.json({ file: req.file });
   res.redirect("/");
 });
